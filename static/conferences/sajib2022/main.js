dd3cd52dd26675c4f22a54de371dbf4b72d3a735
Carla Luciana Padilla Franzotti <49074988+CLPF@users.noreply.github.com>
2022-09-17 02:42:12 -0300

    7SAJIB2022

---
+++ static/conferences/sajib2022/main.js
@@ -0,0 +1,80 @@
+$( function() {
+    
+
+    var date = Date.now(),
+    second = 1000;
+
+    function pad(num) {
+        return ('0' + num).slice(-2);
+    }
+
+    function checkTime(what = "day") {
+
+        var dateObj;
+        date += second;
+        dateObj = new Date(date);
+
+        if (what == "day"){
+            
+            dd = pad(dateObj.getDate()) 
+            mm = pad(parseInt(dateObj.getMonth())+1).toString()
+            yy = dateObj.getFullYear()
+            
+            //clockEl.innerHTML = [dd,mm,yy].join("-");
+            return [dd,mm,yy].join("-");
+        
+        } else {
+        
+            hh = pad(dateObj.getHours())
+            mm = pad(dateObj.getMinutes())
+            ss = pad(dateObj.getSeconds())
+
+            return [hh,mm,ss].join(":");
+
+        }
+    
+
+    } 
+        
+
+    $("#fullprogram").click(function(e){
+        e.preventDefault()
+        $("#day-0").css("display","none")  
+        $("#day-1").css("display","block")
+        $("#day-2").css("display","block")
+        $("#poster").css("display","none")              
+
+    });
+
+    $("#day-1-bt").click(function(e){
+        // toggle day program
+        e.preventDefault()
+        $("#day-0").css("display","none")  
+        $("#day-2").css("display","none")   
+        $("#poster").css("display","none")      
+        $("#day-1").css("display","none")              
+    });
+
+    $("#day-2-bt").click(function(e){
+        // toggle day program
+        e.preventDefault()
+        $("#day-0").css("display","none")  
+        $("#day-1").css("display","none")  
+        $("#poster").css("display","none")      
+        $("#day-2").css("display","none")              
+    });
+
+    $("#poster-bt").click(function(e){
+        // toggle day program
+        e.preventDefault()
+        $("#day-0").css("display","none")  
+        $("#day-1").css("display","none")  
+        $("#day-2").css("display","none")              
+        $("#poster").css("display","none")              
+    });
+
+    setInterval(checkTime, second*60);
+    
+})
+
+
